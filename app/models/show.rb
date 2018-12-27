48dc44c0de7a3c8c5b631dd44c70ce6e6985f583
Dan Hintz <dhintz@live.com>
2018-12-27 19:10:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   
-  def build_network(args)
+  def build_network(:call_letters)
     Network.create(args)
     
   end
