dc94135c96e221b0c5fa9b29a39b61b3c92f5c1e
Dan Hintz <dhintz@live.com>
2018-12-27 19:14:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   
   def build_network(:call_letters, :channel)
     Network.create do |n|
-      n.call_letters = a[:call_letters]
+      n.call_letters = :call_letters
     
   end
 end
