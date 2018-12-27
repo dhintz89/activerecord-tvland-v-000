e2502581462a1982e7d9d2f37bdb3abe67a0f9fe
Dan Hintz <dhintz@live.com>
2018-12-27 18:37:06 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,11 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
+  
+  def list_roles
+    self.characters
+  end
+  
+  def full_name
+    self.first_name
 end
